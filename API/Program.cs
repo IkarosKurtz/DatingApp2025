713c8de9aa443c78c83f3b8523d74adf9263e90a
Ikaros <qwertypopsdown@gmail.com>
2025-08-28 07:45:47 -0600

    feat(parcial01): :sparkles: First EF Migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
