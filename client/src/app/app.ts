e6a85ba411e970bf6fe15c69bb367be9e348749c
Ikaros <qwertypopsdown@gmail.com>
2025-09-03 07:52:28 -0600

    feat(parcial01): :sparkles: App http client to SPA

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private http: HttpClient = inject(HttpClient);
+  protected readonly title = signal('My App');
+
+  public ngOnInit(): void {
+    this.http.get('https://localhost:5001/members').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.error(err),
+      complete: () => console.log('Completed'),
+    });
+  }
 }
