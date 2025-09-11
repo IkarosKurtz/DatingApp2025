e4cee9d61c47252c525f3e7c795d8c39c18df511
Ikaros <qwertypopsdown@gmail.com>
2025-09-11 07:34:50 -0600

    feat(parcial02): :sparkles: Add email verification and better register request dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -8,15 +8,17 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(AppDbContext context) : BaseApiController
 {
     [HttpPost("register")]
-
     public async Task<ActionResult<AppUser>> Register(RegisterRequest request)
     {
+        if (await this.EmailExists(request.Email)) return BadRequest("Email is already taken");
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
@@ -31,5 +33,10 @@ public class AccountController(AppDbContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    public async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
     }
 }
--- API/DTOs/RegisterRequest.cs
+++ API/DTOs/RegisterRequest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace API.DTOs;
 
 public class RegisterRequest
 {
-    public required string DisplayName { get; set; }
-    public required string Email { get; set; }
-    public required string Password { get; set; }
+    [Required]
+    public string DisplayName { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; } = string.Empty;
 }
