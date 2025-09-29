3c5791d6d329691b2882dccab846c1b50237ce31
Ikaros <qwertypopsdown@gmail.com>
2025-09-29 07:50:24 -0600

    feat(parcial02): :sparkles: Add hook to API register

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,19 +1,29 @@
-import { Component, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RegisterCreds } from '../../../types/registerCreds';
+import { Component, inject, output } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AccountService } from "../../../core/services/account-service";
+import { RegisterCreds } from "../../../types/registerCreds";
 
 @Component({
-  selector: 'app-register',
+  selector: "app-register",
   imports: [FormsModule],
-  templateUrl: './register.html',
-  styleUrl: './register.css',
+  templateUrl: "./register.html",
+  styleUrl: "./register.css",
 })
 export class Register {
+  private accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   protected creds: RegisterCreds = {} as RegisterCreds;
 
   public register(): void {
-    console.log(this.creds);
+    this.accountService.register(this.creds).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.cancel();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
   public cancel(): void {
