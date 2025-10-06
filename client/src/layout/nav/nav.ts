a00628fc9f941a201ca31e2e9f4ce841f2ed9724
Ikaros <qwertypopsdown@gmail.com>
2025-10-06 07:24:23 -0600

    feat(parcial02): :sparkles: Add nav links

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,23 +1,25 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AccountService } from '../../core/services/account-service';
+import { Component, inject, signal, WritableSignal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { AccountService } from "../../core/services/account-service";
 @Component({
-  selector: 'app-nav',
-  imports: [FormsModule],
-  templateUrl: './nav.html',
-  styleUrl: './nav.css',
+  selector: "app-nav",
+  imports: [FormsModule, RouterLink, RouterLinkActive],
+  templateUrl: "./nav.html",
+  styleUrl: "./nav.css",
 })
 export class Nav {
+  private router = inject(Router);
   protected accountService = inject(AccountService);
   protected creds: any = {};
   protected isLoggedIn: WritableSignal<boolean> = signal(false);
 
   public login() {
     this.accountService
-      .login({ Email: this.creds.email, Password: this.creds.password })
+      .login({ email: this.creds.email, password: this.creds.password })
       .subscribe({
         next: (res) => {
-          console.log(res);
+          this.router.navigateByUrl("/members");
           this.creds = {};
         },
         error: (err) => alert(err.message),
@@ -26,5 +28,6 @@ export class Nav {
 
   public logout() {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
