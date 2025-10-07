4b2b11706bd11df1c69533156762206513b8dd36
Ikaros <qwertypopsdown@gmail.com>
2025-10-07 07:46:06 -0600

    feat(parcial02): :sparkles: Add errors

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { Lists } from "../features/lists/lists";
 import { MemberDetail } from "../features/members/member-detail/member-detail";
 import { MemberList } from "../features/members/member-list/member-list";
 import { Messages } from "../features/messages/messages";
+import { TestErrors } from "../features/test-errors/test-errors";
 
 export const routes: Routes = [
   { path: "", component: Home },
   {
     path: "",
+    runGuardsAndResolvers: "always",
     canActivate: [authGuard],
     children: [
       { path: "members", component: MemberList },
@@ -18,5 +20,6 @@ export const routes: Routes = [
       { path: "messages", component: Messages },
     ],
   },
+  { path: "errors", component: TestErrors },
   { path: "**", component: Home },
 ];
