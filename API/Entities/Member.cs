fea77c3842bfb33d2232945a23374b7ddc2474f2
Ikaros <qwertypopsdown@gmail.com>
2025-10-13 08:00:42 -0600

    feat(parcial02): :sparkles: Add Member Migration

---
+++ API/Entities/Member.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace API.Entities
+{
+  public class Member
+  {
+    public string Id { get; set; } = null!; // This will be the same as AppUser.Id
+    public DateOnly Birthday { get; set; }
+    public required string DisplayName { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public required string Gender { get; set; }
+    public string? Description { get; set; }
+    public required string City { get; set; }
+    public required string Country { get; set; }
+
+    // Navigation Properties
+    [ForeignKey(nameof(Id))]
+    public AppUser User { get; set; } = null!;
+
+  }
+}
