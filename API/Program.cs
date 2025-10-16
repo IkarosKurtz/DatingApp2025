62519b9a2c883ef70e47c24170461a8b016d49b9
Ikaros <qwertypopsdown@gmail.com>
2025-10-16 07:33:16 -0600

    feat(parcial02): :sparkles: Add database migration and user seeding on application startup

--- API/Program.cs
+++ API/Program.cs
@@ -59,4 +59,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+  var context = services.GetRequiredService<AppDbContext>();
+  await context.Database.MigrateAsync();
+  await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "A migration or seeding error occurred.");
+  throw;
+}
+
 app.Run();
