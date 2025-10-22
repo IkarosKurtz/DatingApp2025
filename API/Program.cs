37903fa73d9f114f02f2481a40a0aee6b6ddd27d
Ikaros <qwertypopsdown@gmail.com>
2025-10-22 10:39:07 -0600

    feat(parcial02-extra): :sparkles: Enhance program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using System.Text.Json.Serialization;
 using API.Data;
 using API.Interfaces;
 using API.Middlewares;
@@ -9,44 +11,111 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace API;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>
+[ExcludeFromCodeCoverage]
+public static class Program
 {
-  opt.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection"));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IMembersRepository, MembersRepository>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-  var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException("TokenKey not found in configuration.");
-  options.TokenValidationParameters = new TokenValidationParameters
+  public static void Main(string[] args)
   {
-    ValidateIssuerSigningKey = true,
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-    ValidateIssuer = false,
-    ValidateAudience = false
-  };
-});
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddHealthChecks();
+    AddServiceDefaults(builder);
 
-var app = builder.Build();
+    builder.Services.AddControllers()
+    .AddMvcOptions(options =>
+    {
+      // Add the filter we could have
+    })
+    .AddJsonOptions(options =>
+    {
+      options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(opt =>
-{
-  opt.AllowAnyHeader().AllowAnyOrigin()
-     .WithOrigins(
-      "http://localhost:4200",
-      "https://localhost:4200"
-  );
-});
+    builder.Services.AddMemoryCache();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    AddDbContext(builder);
+    AddScopedServices(builder);
+
+    WebApplication app = builder.Build();
+
+    var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+
+    try
+    {
+      var context = services.GetRequiredService<AppDbContext>();
+      context.Database.Migrate();
+      Task.Run(() => Seed.SeedUsers(context));
+    }
+    catch (Exception ex)
+    {
+      var logger = services.GetRequiredService<ILogger>();
+      logger.LogError(ex, "A migration or seeding error occurred.");
+      throw;
+    }
+
+    app.UseMiddleware<ExceptionMiddleware>();
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseCors(opt =>
+      {
+        opt.AllowAnyHeader().AllowAnyOrigin()
+          .WithOrigins(
+            "http://localhost:4200",
+            "https://localhost:4200"
+        );
+      });
+      app.UseDeveloperExceptionPage();
+    }
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+  }
+
+  private static void AddDbContext(WebApplicationBuilder builder)
+  {
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+    {
+      opt.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection"));
+    });
+  }
+
+  private static void AddScopedServices(WebApplicationBuilder builder)
+  {
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<IMembersRepository, MembersRepository>();
+  }
+
+  private static void AddServiceDefaults(WebApplicationBuilder builder)
+  {
+    builder.Services.AddCors();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+      var tokenKey = builder.Configuration["TokenKey"] ?? throw new ArgumentNullException("TokenKey not found in configuration.");
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+      };
+    });
+
+  }
+}
+
+
+
+
+
+
 
 // No lo queremos por ahora o quizas nunca
 /* // Configure the HTTP request pipeline.
@@ -58,24 +127,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 */
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
 
-var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-try
-{
-  var context = services.GetRequiredService<AppDbContext>();
-  await context.Database.MigrateAsync();
-  await Seed.SeedUsers(context);
-}
-catch (Exception ex)
-{
-  var logger = services.GetRequiredService<ILogger<Program>>();
-  logger.LogError(ex, "A migration or seeding error occurred.");
-  throw;
-}
-
-app.Run();
