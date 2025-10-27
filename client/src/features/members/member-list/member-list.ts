80da3c45159dfdda381ec2a974d5a1e71558a4aa
Ikaros <qwertypopsdown@gmail.com>
2025-10-27 07:49:30 -0600

    feat(parcial03): :sparkles: Add member cards

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -2,11 +2,12 @@ import { AsyncPipe } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { MembersService } from "../../../core/services/members-service";
+import { MemberCard } from "../../../member-card/member-card";
 import { Member } from "../../../types/member";
 
 @Component({
   selector: "app-member-list",
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MemberCard],
   templateUrl: "./member-list.html",
   styleUrl: "./member-list.css",
 })
