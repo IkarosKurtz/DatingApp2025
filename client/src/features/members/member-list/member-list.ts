c536d1fcdb43c21ce990803e80562a6802a7f131
Ikaros <qwertypopsdown@gmail.com>
2025-10-27 07:47:03 -0600

    feat(parcial03): :sparkles: Add member list

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,9 +1,20 @@
-import { Component } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { Observable } from "rxjs";
+import { MembersService } from "../../../core/services/members-service";
+import { Member } from "../../../types/member";
 
 @Component({
   selector: "app-member-list",
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: "./member-list.html",
   styleUrl: "./member-list.css",
 })
-export class MemberList {}
+export class MemberList {
+  private readonly membersService = inject(MembersService);
+  protected members$: Observable<Member[]>;
+
+  constructor() {
+    this.members$ = this.membersService.getMembers();
+  }
+}
