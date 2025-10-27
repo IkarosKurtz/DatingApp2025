06c921b647cd3d72683e073634dafc06802634e8
Ikaros <qwertypopsdown@gmail.com>
2025-10-27 07:56:38 -0600

    feat(parcial03): :sparkles: Add member detail page

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,9 +1,31 @@
-import { Component } from "@angular/core";
+import { AsyncPipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { MembersService } from "../../../core/services/members-service";
+import { Member } from "../../../types/member";
 
 @Component({
   selector: "app-member-detail",
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: "./member-detail.html",
   styleUrl: "./member-detail.css",
 })
-export class MemberDetail {}
+export class MemberDetail {
+  private readonly memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  protected member$?: Observable<Member>;
+
+  public constructor() {
+    this.member$ = this.loadMember();
+  }
+
+  private loadMember(): Observable<Member> | undefined {
+    const id = this.route.snapshot.paramMap.get("id");
+    if (id) {
+      return this.memberService.getMember(id);
+    }
+
+    return;
+  }
+}
