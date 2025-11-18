344e491ad3c89820523b5bd1f404a5dd91b386a4
Ikaros <qwertypopsdown@gmail.com>
2025-11-18 10:53:31 -0600

    feat(parcial03): :sparkles: Add Photos content

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -2,20 +2,24 @@ import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "../../environments/environment";
-import { Member } from "../../types/member";
+import { Member, Photo } from "../../types/member";
 
 @Injectable({
   providedIn: "root",
 })
 export class MembersService {
-  private http = inject(HttpClient);
+  private readonly http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
 
-  getMember(id: string): Observable<Member> {
+  public getMember(id: string): Observable<Member> {
     return this.http.get<Member>(this.baseUrl + "members/" + id);
   }
 
-  getMembers(): Observable<Member[]> {
+  public getMembers(): Observable<Member[]> {
     return this.http.get<Member[]>(this.baseUrl + "members");
+  }
+
+  public getPhoto(id: string): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
   }
 }
