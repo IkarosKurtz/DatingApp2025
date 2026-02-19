2011de09e4ce5bb29237fc4c7d1884e60638a547
IkarosKurtz <qwertypopsdown@gmail.com>
2026-02-18 22:46:04 -0600

    feat(parcial04): :sparkles: Add loading interceptor

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -8,6 +8,7 @@ import {
 import { FormsModule } from "@angular/forms";
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { AccountService } from "../../core/services/account-service";
+import { BusyService } from "../../core/services/busy-service";
 import { ToastService } from "../../core/services/toast-service";
 import { themes } from "../theme";
 @Component({
@@ -17,13 +18,14 @@ import { themes } from "../theme";
   styleUrl: "./nav.css",
 })
 export class Nav implements OnInit {
-  private router = inject(Router);
-  private toast = inject(ToastService);
-  protected accountService = inject(AccountService);
+  private readonly routerService = inject(Router);
+  private readonly toastService = inject(ToastService);
+  protected readonly busyService = inject(BusyService);
+  protected readonly accountService = inject(AccountService);
   protected creds: any = {};
   protected isLoggedIn: WritableSignal<boolean> = signal(false);
   protected selectedTheme = signal<string>(
-    localStorage.getItem("Theme") || "light"
+    localStorage.getItem("Theme") || "light",
   );
   protected themes = themes;
 
@@ -36,19 +38,19 @@ export class Nav implements OnInit {
       .login({ email: this.creds.email, password: this.creds.password })
       .subscribe({
         next: (res) => {
-          this.router.navigateByUrl("/members");
+          this.routerService.navigateByUrl("/members");
           this.creds = {};
-          this.toast.success("Logged In!");
+          this.toastService.success("Logged In!");
         },
         error: (err) => {
-          this.toast.error(err.error);
+          this.toastService.error(err.error);
         },
       });
   }
 
   public logout() {
     this.accountService.logout();
-    this.router.navigateByUrl("/");
+    this.routerService.navigateByUrl("/");
   }
 
   public handleSelectedTheme(theme: string) {
