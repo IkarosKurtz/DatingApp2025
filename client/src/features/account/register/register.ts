30dc44fb866f88bca8ce936002fa6dfabe5a6c94
Ikaros <qwertypopsdown@gmail.com>
2026-04-16 22:45:33 -0600

    feat(parcial05): :sparkles: Add validators to reactive form

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,11 @@
 import { JsonPipe } from "@angular/common";
 import { Component, inject, OnInit, output } from "@angular/core";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { AccountService } from "../../../core/services/account-service";
 import { RegisterCreds } from "../../../types/user";
 
@@ -16,16 +21,26 @@ export class Register implements OnInit {
   protected registerForm: FormGroup = new FormGroup({});
   cancelRegister = output<boolean>();
 
+  private readonly EMAIL_REGEX =
+    "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
   ngOnInit(): void {
     this.initializeForm();
   }
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl(),
-      displayName: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl(),
+      email: new FormControl("", [
+        Validators.required,
+        Validators.pattern(this.EMAIL_REGEX),
+      ]),
+      displayName: new FormControl("", [Validators.required]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl("", [Validators.required]),
     });
   }
 
@@ -42,7 +57,7 @@ export class Register implements OnInit {
     // });
   }
 
-  public cancel(): void {
+  cancel(): void {
     this.cancelRegister.emit(false);
   }
 }
