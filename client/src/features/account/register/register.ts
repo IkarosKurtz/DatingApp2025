1d4f414976d48ba817cb27f24210b745f00d3a44
Ikaros <qwertypopsdown@gmail.com>
2026-04-16 22:53:35 -0600

    feat(parcial05): :sparkles: Add form Steps

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from "@angular/common";
-import { Component, inject, output } from "@angular/core";
+import { Component, inject, output, signal } from "@angular/core";
 import {
   AbstractControl,
   FormBuilder,
@@ -23,12 +23,22 @@ export class Register {
   private accountService = inject(AccountService);
   private fb = inject(FormBuilder);
   protected creds = {} as RegisterCreds;
-  protected registerForm: FormGroup;
+  protected credentialsForm: FormGroup;
+  protected profileForm: FormGroup;
+  protected currentStep = signal(1);
   cancelRegister = output<boolean>();
 
   constructor() {
-    this.registerForm = this.fb.group({
-      email: ["", [Validators.required, Validators.email]],
+    this.credentialsForm = this.fb.group({
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$",
+          ),
+        ],
+      ],
       displayName: ["", [Validators.required]],
       password: [
         "",
@@ -39,8 +49,16 @@ export class Register {
         [Validators.required, this.matchValues("password")],
       ],
     });
-    this.registerForm.controls["password"].valueChanges.subscribe(() => {
-      this.registerForm.controls["confirmPassword"].updateValueAndValidity();
+
+    this.profileForm = this.fb.group({
+      gender: ["", Validators.required],
+      birthDay: ["", Validators.required],
+      city: ["", Validators.required],
+      country: ["", Validators.required],
+    });
+
+    this.credentialsForm.controls["password"].valueChanges.subscribe(() => {
+      this.credentialsForm.controls["confirmPassword"].updateValueAndValidity();
     });
   }
 
@@ -53,9 +71,34 @@ export class Register {
     };
   }
 
+  nextStep() {
+    if (this.credentialsForm.valid) {
+      this.currentStep.update((prevStep) => prevStep + 1);
+    }
+  }
+
+  prevStep() {
+    this.currentStep.update((prevStep) => prevStep - 1);
+  }
+
+  getMaxDate() {
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    return today.toISOString().split("T")[0];
+  }
+
   register(): void {
     console.group("REGISTER");
-    console.log(this.registerForm.value);
+    console.log(this.credentialsForm.value);
+    console.log(this.profileForm.value);
+    console.log(this.currentStep());
+    if (this.credentialsForm.valid && this.profileForm.valid) {
+      const formData = {
+        ...this.credentialsForm.value,
+        ...this.profileForm.value,
+      };
+      console.log("Form data:", formData);
+    }
     console.groupEnd();
     // this.accountService.register(this.creds).subscribe({
     //   next: response => {
