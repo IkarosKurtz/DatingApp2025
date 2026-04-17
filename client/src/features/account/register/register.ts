61b44085d7351dc2bfbd295e3797c72b4326e96d
Ikaros <qwertypopsdown@gmail.com>
2026-04-16 22:46:19 -0600

    feat(parcial05): :sparkles: Add custom validator

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,9 +1,12 @@
 import { JsonPipe } from "@angular/common";
 import { Component, inject, OnInit, output } from "@angular/core";
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from "@angular/forms";
 import { AccountService } from "../../../core/services/account-service";
@@ -40,8 +43,24 @@ export class Register implements OnInit {
         Validators.minLength(4),
         Validators.maxLength(8),
       ]),
-      confirmPassword: new FormControl("", [Validators.required]),
+      confirmPassword: new FormControl("", [
+        Validators.required,
+        this.matchValues("password"),
+      ]),
     });
+
+    this.registerForm.controls["password"].valueChanges.subscribe(() => {
+      this.registerForm.controls["confirmPassword"].updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent;
+      if (!parent) return null;
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : { passwordMismatch: true };
+    };
   }
 
   register(): void {
