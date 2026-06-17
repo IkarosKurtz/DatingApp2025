9f6eaeefa56c148fa0782045427961ace2576cab
Ikaros <qwertypopsdown@gmail.com>
2026-06-17 15:17:14 -0600

    feat(parcial06/signal-r): :sparkles: Update message to use message groups

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -54,6 +54,7 @@ public static class Program
       var context = services.GetRequiredService<AppDbContext>();
       var userManager = services.GetRequiredService<UserManager<AppUser>>();
       context.Database.Migrate();
+      context.Connections.ExecuteDelete();
       Task.Run(() => Seed.SeedUsers(userManager));
     }
     catch (Exception ex)
