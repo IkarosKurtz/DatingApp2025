ea10ed9033d6bcea26528d4844e4909d425d9334
Ikaros <qwertypopsdown@gmail.com>
2026-06-17 14:52:13 -0600

    feat(parcial06/signal-r): :sparkles: Add presence hub to client

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,9 +1,11 @@
-import { HttpClient } from "@angular/common/http"
-import { inject, Injectable, signal } from "@angular/core"
-import { Observable, tap } from "rxjs"
-import { environment } from "../../environments/environment"
-import { LoginCreds, RegisterCreds, User } from "../../types/user"
-import { LikesService } from "./likes-service"
+import { HttpClient } from '@angular/common/http'
+import { inject, Injectable, signal } from '@angular/core'
+import { HubConnectionState } from '@microsoft/signalr'
+import { Observable, tap } from 'rxjs'
+import { environment } from '../../environments/environment'
+import { LoginCreds, RegisterCreds, User } from '../../types/user'
+import { LikesService } from './likes-service'
+import { PresenceService } from './presence-service'
 
 @Injectable({
   providedIn: "root",
@@ -11,6 +13,7 @@ import { LikesService } from "./likes-service"
 export class AccountService {
   private http = inject(HttpClient);
   private likesService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   currentUser = signal<User | null>(null);
   baseUrl = environment.apiUrl;
 
@@ -57,6 +60,9 @@ export class AccountService {
     user.roles = this.getRolesFromToken(user);
     this.currentUser.set(user);
     this.likesService.getLikeIds();
+    if (this.presenceService.hubConnection?.state !== HubConnectionState.Connected) {
+      this.presenceService.createHubConnection(user);
+    }
   }
 
   logout() {
