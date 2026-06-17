6ff0f96e86d3e4269ca0799845836a33984bbcb7
Ikaros <qwertypopsdown@gmail.com>
2026-06-17 13:37:28 -0600

    feat(parcial06): :sparkles:
    Update messages table with styling

--- client/src/features/messages/messages.ts
+++ client/src/features/messages/messages.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common'
 import { Component, inject, OnInit, signal } from '@angular/core'
 import { RouterLink } from '@angular/router'
 import { MessagesService } from '../../core/services/messages-service'
@@ -7,13 +8,14 @@ import { PaginationResult } from '../../types/paginationMetadata'
 
 @Component({
   selector: 'app-messages',
-  imports: [Paginator, RouterLink],
+  imports: [Paginator, RouterLink, DatePipe],
   templateUrl: './messages.html',
   styleUrl: './messages.css'
 })
 export class Messages implements OnInit {
   private messagesService = inject(MessagesService);
-  protected container = 'inbox';
+  protected container = 'Inbox';
+  protected fetchedContainer = 'Inbox';
   protected pageNumber = 1;
   protected pageSize = 10;
   protected paginatedMessages = signal<PaginationResult<Message> | null>(null);
@@ -29,12 +31,15 @@ export class Messages implements OnInit {
 
   loadMessages() {
     this.messagesService.getMessages(this.container, this.pageNumber, this.pageSize).subscribe({
-      next: response => this.paginatedMessages.set(response)
+      next: response => {
+        this.paginatedMessages.set(response);
+        this.fetchedContainer = this.container;
+      }
     });
   }
 
   get isInbox() {
-    return this.container === 'Inbox';
+    return this.fetchedContainer === 'Inbox';
   }
 
   setContainer(container: string) {
