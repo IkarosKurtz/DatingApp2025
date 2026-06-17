526a36be00c03038c4a60740d0142e87260e75c2
Ikaros <qwertypopsdown@gmail.com>
2026-06-17 15:17:44 -0600

    fix(parcial06/signal-r): :bug: Fix to GetUserID

--- src/API/SignalR/MessageHub.cs
+++ src/API/SignalR/MessageHub.cs
@@ -1,6 +1,6 @@
-using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using API.Mappers;
 using API.Repositories.Interfaces;
@@ -88,6 +88,6 @@ public class MessageHub(IMessagesRepository messagesRepository,
 
   private string GetUserId()
   {
-    return Context.User?.FindFirstValue(ClaimTypes.Email) ?? throw new HubException("Cannot get member id");
+    return Context.User?.GetMemberId() ?? throw new HubException("Cannot get member id");
   }
 }
